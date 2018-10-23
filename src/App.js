712fe41e6f9edd9b692a1fdc4c15dafc4d2d29cb
Tim Biles <timbilestimbiles@gmail.com>
2018-10-23 10:02:14 -0500

    after review

--- src/App.js
+++ src/App.js
@@ -3,23 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    sideNav: false,
+    links: ['Home', 'About', 'Contact']
+  };
+
   render() {
+    const { sideNav, links } = this.state;
+
+    const linkMap = links.map((e, i) => {
+      return (
+        <div key={i}>
+          <p>{e}</p>
+        </div>
+      );
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <nav className="nav">
+          <h2>Menu</h2>
+          <div className="main_nav">{linkMap}</div>
+          <div className="hamburger">
+            <div
+              className="hamburger_icon"
+              onClick={() => this.setState({ sideNav: !sideNav })}
+            >
+              <div className="burger burger1" />
+              <div className="burger burger2" />
+              <div className="burger burger3" />
+            </div>
+            {sideNav && <div className="sidenav">{linkMap}</div>}
+          </div>
+        </nav>
+        <div className="loading_bar">
+          <h2>Loading bar</h2>
+          <div className="container">
+            <div className="bar first" />
+            <div className="bar second" />
+            <div className="bar third" />
+            <div className="bar fourth" />
+            <div className="bar fifth" />
+          </div>
+        </div>
       </div>
     );
   }
